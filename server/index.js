7dff2cec12bcb4bf9b42a8f90317efdca1f3d572
goelharsh <harshgoel18122001@gmail.com>
2024-04-14 16:54:31 +0530

    bakendurl

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://smartstudy-mydh.onrender.com",
         credentials:true
     })
 )
